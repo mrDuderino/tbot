409a1c1cbad1b46127c31f8d6c37a16b00e7c9b7
Vladimir <capcapmor@gmail.com>
2023-02-05 14:17:45 +0300

    Add /help command

--- cmd/bot/main.go
+++ cmd/bot/main.go
@@ -19,10 +19,8 @@ func main() {
 	}
 
 	bot.Debug = true
-
 	log.Printf("Authorized on account %s", bot.Self.UserName)
 
-	//u := tgbotapi.NewUpdate(0)
 	u := tgbotapi.UpdateConfig{
 		Offset:  0,
 		Timeout: 60,
@@ -31,13 +29,28 @@ func main() {
 	updates := bot.GetUpdatesChan(u)
 
 	for update := range updates {
-		if update.Message != nil { // If we got a message
-			log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
+		if update.Message == nil {
+			continue
+		}
 
-			msg := tgbotapi.NewMessage(update.Message.Chat.ID, update.Message.Text)
-			msg.ReplyToMessageID = update.Message.MessageID
-
-			bot.Send(msg)
+		switch update.Message.Command() {
+		case "help":
+			helpCommand(bot, update.Message)
+		default:
+			defaultBehavior(bot, update.Message)
 		}
 	}
+}
+
+func helpCommand(bot *tgbotapi.BotAPI, inputMessage *tgbotapi.Message) {
+	msg := tgbotapi.NewMessage(inputMessage.Chat.ID, "This is a test bot developed on GO")
+	bot.Send(msg)
+}
+
+func defaultBehavior(bot *tgbotapi.BotAPI, inputMessage *tgbotapi.Message) {
+	log.Printf("[%s] %s", inputMessage.From.UserName, inputMessage.Text)
+
+	msg := tgbotapi.NewMessage(inputMessage.Chat.ID, inputMessage.Text)
+	msg.ReplyToMessageID = inputMessage.MessageID
+	bot.Send(msg)
 }
